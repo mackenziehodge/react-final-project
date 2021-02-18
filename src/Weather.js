f1a7c8106ab752598fa7b732527ebd8619841fcf
mackenziehodge <72561318+mackenziehodge@users.noreply.github.com>
2021-02-18 18:37:16 -0500

    fixed the positioning of the icon image

--- src/Weather.js
+++ src/Weather.js
@@ -18,10 +18,20 @@ return( <div className="Weather">
 <ul><li>Wednesday 10:00</li>
 <li>Scatterd Clouds</li></ul>
 
-<div className="row">
+<div className="row mt-4">
     <div className="col-6">
-        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="scattered clouds"/>
-   <h2>-4°C</h2> </div>
+      <div className="clearfix">
+            <img 
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+            alt="scattered clouds" 
+            className="float-left"
+            />
+<div className="float-left">
+   <span className="temperature">-4</span>
+   <span className="unit">°C</span> 
+   </div>
+   </div>
+   </div>
    <div className="col-6">
        <ul><li>Precipitation 15%</li>
        <li>Humidity:98%</li>
