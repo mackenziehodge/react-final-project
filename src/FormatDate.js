b8312ecbc7c520072b47fc9d2a6994eefd6d6e7f
mackenziehodge <72561318+mackenziehodge@users.noreply.github.com>
2021-02-18 21:22:35 -0500

    added the date and year with real data

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default function FormatDate(props){
     let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let day=days[props.date.getDay()];
+    let months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+let date=props.date.getDate();
+    let day=days[props.date.getDay()];
 let hours =props.date.getHours();
 if (hours <10){
     hours=`0${hours}`
@@ -11,8 +13,11 @@ let minutes=props.date.getMinutes();
 if (minutes <10){
     minutes=`0${minutes}`
 }
+let month=months[props.date.getMonth()];
 
-    return <div>{day} {hours}:{minutes} </div>;
+let year=props.date.getFullYear();
+
+    return <div>{day} {month} {date}, {year}<br/> {hours}:{minutes}  </div>;
 
 
 }
